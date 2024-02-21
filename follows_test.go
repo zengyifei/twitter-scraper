5d3a5724f9d8fa9c497febeb7c4493b80ec8bc0f
Valentine <xb2w1z@gmail.com>
2024-02-21 06:15:04 +0300

    add followers method

--- follows_test.go
+++ follows_test.go
@@ -1,11 +1,23 @@
 package twitterscraper_test
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestFetchFollowing(t *testing.T) {
 	users, _, err := testScraper.FetchFollowing("Support", 20, "")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(users[0].Username)
+	if len(users) < 1 || users[len(users)-1].Username == "" {
+		t.Error("error FetchFollowing() No users found")
+	}
+}
+
+func TestFetchFollowers(t *testing.T) {
+	users, _, err := testScraper.FetchFollowers("Support", 20, "")
 	if err != nil {
 		t.Error(err)
 	}
