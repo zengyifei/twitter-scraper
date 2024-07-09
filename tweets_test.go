c9696514c4adff2e77f5f95c620110cd111ea970
Valentine <xb2w1z@gmail.com>
2024-07-09 05:20:49 +0300

    added foryou timeilne

--- tweets_test.go
+++ tweets_test.go
@@ -378,3 +378,76 @@ func TestGetHomeTweets(t *testing.T) {
 		t.Errorf("Expected tweets count=%v, got: %v", maxTweetsNbr, count)
 	}
 }
+
+func TestFetchForYouTweets(t *testing.T) {
+	if skipAuthTest {
+		t.Skip("Skipping test due to environment variable")
+	}
+	tweets, _, err := testScraper.FetchForYouTweets("", 20, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(tweets) < 1 {
+		t.Fatal("returned 0 tweets")
+	}
+}
+
+func TestGetForYouTweets(t *testing.T) {
+	if skipAuthTest {
+		t.Skip("Skipping test due to environment variable")
+	}
+	count := 0
+	maxTweetsNbr := 150
+	dupcheck := make(map[string]bool)
+
+	for tweet := range testScraper.GetForYouTweets(context.Background(), maxTweetsNbr) {
+		if tweet.Error != nil {
+			t.Error(tweet.Error)
+		} else {
+			count++
+			if tweet.ID == "" {
+				t.Error("Expected tweet ID is empty")
+			} else {
+				if dupcheck[tweet.ID] {
+					t.Errorf("Detect duplicated tweet ID: %s", tweet.ID)
+				} else {
+					dupcheck[tweet.ID] = true
+				}
+			}
+			if tweet.UserID == "" {
+				t.Error("Expected tweet UserID is empty")
+			}
+			if tweet.Username == "" {
+				t.Error("Expected tweet Username is empty")
+			}
+			if tweet.PermanentURL == "" {
+				t.Error("Expected tweet PermanentURL is empty")
+			}
+			if tweet.Text == "" {
+				t.Error("Expected tweet Text is empty")
+			}
+			if tweet.TimeParsed.IsZero() {
+				t.Error("Expected tweet TimeParsed is zero")
+			}
+			if tweet.Timestamp == 0 {
+				t.Error("Expected tweet Timestamp is greater than zero")
+			}
+			for _, video := range tweet.Videos {
+				if video.ID == "" {
+					t.Error("Expected tweet video ID is empty")
+				}
+				if video.Preview == "" {
+					t.Error("Expected tweet video Preview is empty")
+				}
+				if video.URL == "" {
+					t.Error("Expected tweet video URL is empty")
+				}
+			}
+		}
+	}
+
+	if count != maxTweetsNbr {
+		t.Errorf("Expected tweets count=%v, got: %v", maxTweetsNbr, count)
+	}
+}
